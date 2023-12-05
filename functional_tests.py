138f627b5e2c06a3fbc9a3a8976d91f500210491
shohitomi <shohitomi25@gmail.com>
2023-12-05 10:32:23 +0900

    rename filename

---
+++ functional_tests.py
@@ -0,0 +1,37 @@
+import unittest
+from selenium import webdriver
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a cool new online to-do app
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+
+        # She types "Buiy peacock feathers" into a text box
+        # (Edith's hobby is typing fly-finshing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item
+        # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
+    
